baf2c6d3acaaffb2a435a3e38c3cf2c28fd6e2e1
Gema Ramadhan <gemaramadhan201@gmail.com>
2022-10-05 17:03:01 +0700

    Add project files.

---
+++ Bubblesort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bubble_sort
+{
+    class program
+    {
+        //deklarasi array ini dengan ukuran 20
+        private int[] a = new int[20],
+
+        //deklarasi variabel ini untuk menyimpan data pada array
+        private int[] n;
+
+        //fungsi / Method untuk menerima masukan
+        public void read()
+        {
+
+        }
+    }
+}
